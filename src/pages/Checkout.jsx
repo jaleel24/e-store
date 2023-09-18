0e3c011b674319d0df10134ad91a85b4b81826c9
jaleel sayal <jaleelsayal@hotmail.com>
2023-09-18 18:21:24 -0400

    added more validations to the form, but are basic ones

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -8,38 +8,49 @@ const Checkout = () => {
     email: "",
     billingAddress: "",
   });
+  const[isTouched , setIsTouched] =  useState(true);
   const navigate = useNavigate();
+  //! we are using the below code to manupilate the placeorder button once all the values return true meaning nothing is in there name , email, address ,its length === 0 then
+  //! place order button will be disables and once we have all the inputs it will be enabled, we want to check it on every rerender thats another reason of this code
+  //! now we are going to map it with the button
+  const errors = {
+    name: form.name.length === 0,
+    email: form.email.length === 0,
+    billingAddress: form.billingAddress.length === 0,
+  };
+ 
+  const disabled = Object.keys(errors).some((x) => errors[x]);
+  
 
   const formSubmitHandler = (ev) => {
-    if (formInvalid()) {
+    ev.preventDefault();
+    if (disabled) {
       ev.preventDefault();
       return;
     }
+    
     navigate("/orderConfirmation");
   };
 
   const handleChange = (ev) => {
+    
     const { name, value } = ev.target;
-
+    //setIsTouched(true)
     setForm((prevState) => {
       return {
         ...prevState,
         [name]: value,
       };
+      
     });
+    //setIsTouched(false);
   };
 
-  const formInvalid = () => {
-    const errors = {
-      name: form.name.length === 0,
-      email: form.email.length === 0,
-      billingAddress: form.billingAddress.length === 0,
-    };
-
-    const disabled = Object.keys(errors).some((x) => errors[x]);
-    return disabled;
-  };
-
+  
+const invalidName = errors.name ? 'bg-red-200 w-full p-2 border rounded-md' : 'bg-white w-full p-2 border rounded-md';
+const invalidEmail = errors.email ? 'bg-red-200 w-full p-2 border rounded-md' : 'bg-white w-full p-2 border rounded-md';
+const invalidSBilling = errors.billingAddress ? 'bg-red-200 w-full p-2 border rounded-md' : 'bg-white w-full p-2 border rounded-md';
+//const invalidName = errors.name ? 'bg-red-200 w-full p-2 border rounded-md' : 'bg-white w-full p-2 border rounded-md';
   return (
     <>
       <form onSubmit={formSubmitHandler}>
@@ -56,47 +67,55 @@ const Checkout = () => {
               <input
                 name="name"
                 onChange={handleChange}
-                className="w-full p-2 border rounded-md"
+                className={invalidName}
                 placeholder="John Doe"
+                
               />
+           
+               {errors.name && <p className="text-red-500 ">Enter the name please: </p>}
             </div>
             <div className="mb-4">
               <label className="block font-medium mb-1">Email</label>
               <input
                 name="email"
                 onChange={handleChange}
-                className="w-full p-2 border rounded-md"
+                className={invalidEmail}
                 placeholder="doe@gmail.com"
               />
             </div>
-            <div className="mb-4">
+            {errors.email && <p className="text-red-500 ">Enter the email please: </p>}
+            {/* <div className="mb-4">
               <label className="block font-medium mb-1">Shipping Address</label>
               <input
                 name="shippingAddress"
                 onChange={handleChange}
-                className="w-full p-2 border rounded-md"
+                className={invalidSBilling}
                 placeholder="warrington"
               />
-            </div>
+            </div> */}
             <div className="mb-4">
               <label className="block font-medium mb-1">Billing Address</label>
               <input
                 name="billingAddress"
                 onChange={handleChange}
-                className="w-full p-2 border rounded-md"
+                className={invalidSBilling}
                 placeholder="L9S-2C6 Warrington"
               />
             </div>
+            {errors.billingAddress && <p className="text-red-500 ">Enter the Billing address: </p>}
             <div className="text-center space-x-9">
-              <button className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
-                Place Order
-              </button>
               <button
                 onClick={() => navigate("/basket")}
                 className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
               >
                 Cancel
               </button>
+             { !disabled && <button
+               
+                className="px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              >
+                Place Order
+              </button>}
             </div>
           </div>
         </div>
